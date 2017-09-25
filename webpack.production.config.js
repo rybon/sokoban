0952e1c2067e5dd3b6d0296e1c393bcbccf6a087
Ryan Bonte <ryan.bonte@gmail.com>
2017-09-25 16:02:21 +0200

    use webpack for compression

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -2,6 +2,7 @@ const webpack = require('webpack')
 const webpackConfig = require('./webpack.config')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const CompressionPlugin = require('compression-webpack-plugin')
 
 delete webpackConfig.devtool
 webpackConfig.entry = webpackConfig.entry[webpackConfig.entry.length - 1]
@@ -13,7 +14,14 @@ webpackConfig.plugins = [
   }),
   new OptimizeCssAssetsPlugin(),
   new webpack.optimize.UglifyJsPlugin(),
-  webpackConfig.plugins[webpackConfig.plugins.length - 1]
+  webpackConfig.plugins[webpackConfig.plugins.length - 1],
+  new CompressionPlugin({
+    asset: '[path].gz[query]',
+    algorithm: 'gzip',
+    test: /\.(js|css|html)$/,
+    threshold: 10240,
+    minRatio: 0.8
+  })
 ]
 const use =
   webpackConfig.module.rules[webpackConfig.module.rules.length - 1].use
