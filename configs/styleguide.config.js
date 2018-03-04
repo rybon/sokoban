70a16297be9c1538d8faac6133bd88d6c8f0d880
Ryan Bonte <ryan.bonte@gmail.com>
2018-03-04 23:54:35 +0100

    fixed build

--- configs/styleguide.config.js
+++ configs/styleguide.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   components: '../src/components/presentational/**/*.jsx',
   webpackConfig: require('./webpack.config.js'), // eslint-disable-line global-require
+  styleguideDir: '../dist/styleguide',
   showCode: true,
   showUsage: true
 }
